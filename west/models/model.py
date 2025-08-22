fb68df99b0ec9975f768e9df88477912c0a7575a
cdliang11 <1404056823@qq.com>
2025-08-22 17:37:50 +0800

    [fix] fix typo

--- west/models/model.py
+++ west/models/model.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 @dataclass
 class ModelArgs:
-    model_type: str = 'speech_llm'
+    model_type: str = 'torch_asu'
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
