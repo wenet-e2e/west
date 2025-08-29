[touchchat] decode runs ok, but shows wrong result

west/models/touch_chat/extractor_touch_chat.py
@@ -22,6 +22,9 @@ class ExtractorTouchChat(Extractor):
     def __init__(self, tokenizer, inference=False):
         super().__init__(tokenizer, inference)
         self.asu_extractor = ExtractorTouchASU(tokenizer, inference)
+        if self.inference:
+            self.fields_batch_static.remove('talker_offsets')
+            self.fields_batch_dynamic.remove('talker_features')
 
     def extract(self, item):
         import s3tokenizer
@@ -32,29 +35,32 @@ class ExtractorTouchChat(Extractor):
         # {'role': 'assistant',
         #  'content': {'type': 'audio', 'audio', x, 'text':x}}
         # Change assistant audio output to text output for Thinker(ASU)
-        asu_item = copy.deepcopy(item)
-        assert len(asu_item['messages']) == 2
-        assert asu_item['messages'][1]['role'] == 'assistant'
-        assistant_text = asu_item['messages'][1]['content']['text']
-        asu_item['messages'][1]['content'] = assistant_text
-        ret = self.asu_extractor.extract(asu_item)
-        assert item['messages'][1]['content']['type'] == 'audio'
-        audio_file = item['messages'][1]['content']['audio']
-        wav, sample_rate = torchaudio.load(audio_file)
-        wav = torchaudio.transforms.Resample(sample_rate, 16000)(wav)
-        wav = wav[0]  # get the first channel
-        mel = s3tokenizer.log_mel_spectrogram(wav)
-        mel = mel.transpose(0, 1)
-        # There is 100 frames mel per second, and 25 tokens per second
-        num_audio_token = math.ceil(mel.size(0) / 100.0 * 25)
-        ids = [0] * num_audio_token + [self.tokenizer.eos_token_id]
-        ids_audio = torch.tensor(ids, dtype=torch.long)
-        # We first ignore it in thinker, then override `tgt_audio` in talker
-        tgt = [IGNORE_TOKEN_ID] * num_audio_token + [
-            self.tokenizer.eos_token_id
-        ]
-        tgt_audio = torch.tensor(tgt, dtype=torch.long)
-        if not self.inference:
+        if self.inference:
+            ret = self.asu_extractor.extract(item)
+            return ret
+        else:
+            asu_item = copy.deepcopy(item)
+            assert len(asu_item['messages']) == 2
+            assert asu_item['messages'][1]['role'] == 'assistant'
+            assistant_text = asu_item['messages'][1]['content']['text']
+            asu_item['messages'][1]['content'] = assistant_text
+            ret = self.asu_extractor.extract(asu_item)
+            assert item['messages'][1]['content']['type'] == 'audio'
+            audio_file = item['messages'][1]['content']['audio']
+            wav, sample_rate = torchaudio.load(audio_file)
+            wav = torchaudio.transforms.Resample(sample_rate, 16000)(wav)
+            wav = wav[0]  # get the first channel
+            mel = s3tokenizer.log_mel_spectrogram(wav)
+            mel = mel.transpose(0, 1)
+            # There is 100 frames mel per second, and 25 tokens per second
+            num_audio_token = math.ceil(mel.size(0) / 100.0 * 25)
+            ids = [0] * num_audio_token + [self.tokenizer.eos_token_id]
+            ids_audio = torch.tensor(ids, dtype=torch.long)
+            # We first ignore it in thinker, then override `tgt_audio` in talker
+            tgt = [IGNORE_TOKEN_ID] * num_audio_token + [
+                self.tokenizer.eos_token_id
+            ]
+            tgt_audio = torch.tensor(tgt, dtype=torch.long)
             # Merge Thinker/Talker tokens in TouchChat
             thinker_input_ids = ret['input_ids']
             thinker_labels = ret['labels']
@@ -62,7 +68,7 @@ class ExtractorTouchChat(Extractor):
             ret['talker_offsets'] = len(thinker_input_ids)
             ret['input_ids'] = torch.cat((thinker_input_ids, ids_audio), dim=0)
             ret['labels'] = torch.cat((thinker_labels, tgt_audio), dim=0)
-        return ret
+            return ret
 
 
 if __name__ == '__main__':

west/models/touch_chat/modeling_touch_chat.py
@@ -91,9 +91,50 @@ class TouchChat(PreTrainedModel):
                           audio_features=talker_features,
                           audio_features_lengths=talker_features_lengths,
                           batch_idx=batch_idx,
-                          input_embs=hidden_embs,
+                          inputs_embeds=hidden_embs,
                           **kwargs)
         return out
 
+    @torch.autocast(device_type="cuda", dtype=torch.bfloat16)
+    def generate(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        audio_offsets: Optional[torch.LongTensor] = None,
+        audio_features: Optional[torch.FloatTensor] = None,
+        audio_features_lengths: Optional[torch.LongTensor] = None,
+        batch_idx: Optional[torch.LongTensor] = None,
+        **kwargs,
+    ):
+        thinker_out = self.thinker.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            audio_offsets=audio_offsets,
+            audio_features=audio_features,
+            audio_features_lengths=audio_features_lengths,
+            batch_idx=batch_idx,
+            eos_token_id=self.eos_token_id,
+            return_dict_in_generate=True,
+            output_scores=True,
+            output_hidden_states=True)
+        text_lengths = torch.tensor([len(thinker_out.sequences)],
+                                    dtype=torch.long,
+                                    device=input_ids.device)
+        hidden_state = torch.cat([x[-1] for x in thinker_out.hidden_states],
+                                 dim=1)
+        hidden_embs = self.projector(hidden_state)
+        model_outputs = self.talker.generate(
+            text_lengths=text_lengths,
+            inputs_embeds=hidden_embs,
+            eos_token_id=self.eos_token_id,
+        )
+        return model_outputs
+
     def init_tokenizer(self):
-        return self.thinker.init_tokenizer()
+        # Here we assume thinker and talker shares the same tokenizer
+        tokenizer = self.thinker.init_tokenizer()
+        self.eos_token_id = tokenizer.convert_tokens_to_ids(
+            ['<|endoftext|>', '<|im_end|>'])
+        return tokenizer

west/models/touch_tts/modeling_touch_tts.py
@@ -60,7 +60,7 @@ class TouchTTS(PreTrainedModel):
         audio_features: Optional[torch.FloatTensor] = None,
         audio_features_lengths: Optional[torch.LongTensor] = None,
         batch_idx: Optional[torch.LongTensor] = None,
-        input_embs: Optional[torch.LongTensor] = None,
+        inputs_embeds: Optional[torch.LongTensor] = None,
     ):
         """ Extract speech codes by speech tokenizer, and reorg that in
             `input_ids`, `labels`
@@ -75,14 +75,14 @@ class TouchTTS(PreTrainedModel):
             input_ids[b, s:e] = ids
             labels[b, s:e] = ids
         text_embs = self.llm.get_input_embeddings()(input_ids)
-        if input_embs is None:
+        if inputs_embeds is None:
             return text_embs, labels
         else:  # replace speech token emb
             for i in range(audio_features.size(0)):
                 b = batch_idx[i]
                 s, e = audio_offsets[i], audio_offsets[i] + speech_codes_lens[i]
-                input_embs[b, s:e] = text_embs[b, s:e]
-            return input_embs, labels
+                inputs_embeds[b, s:e] = text_embs[b, s:e]
+            return inputs_embeds, labels
 
     @torch.autocast(device_type="cuda", dtype=torch.bfloat16)
     def forward(
@@ -95,14 +95,14 @@ class TouchTTS(PreTrainedModel):
         audio_features: Optional[torch.FloatTensor] = None,
         audio_features_lengths: Optional[torch.LongTensor] = None,
         batch_idx: Optional[torch.LongTensor] = None,
-        input_embs: Optional[torch.FloatTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
         **kwargs,
     ):
-        input_embs, labels = self.reorg_ids(input_ids, labels, audio_offsets,
-                                            audio_features,
-                                            audio_features_lengths, batch_idx,
-                                            input_embs)
-        out = self.llm(inputs_embeds=input_embs,
+        inputs_embeds, labels = self.reorg_ids(input_ids, labels, audio_offsets,
+                                               audio_features,
+                                               audio_features_lengths,
+                                               batch_idx, inputs_embeds)
+        out = self.llm(inputs_embeds=inputs_embeds,
                        attention_mask=attention_mask,
                        labels=labels,
                        position_ids=position_ids,
@@ -120,19 +120,21 @@ class TouchTTS(PreTrainedModel):
         audio_features_lengths: Optional[torch.LongTensor] = None,
         batch_idx: Optional[torch.LongTensor] = None,
         text_lengths: Optional[torch.LongTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
         eos_token_id=None,
         decode_config=None,
     ):
-        assert input_ids.size(0) == 1
-        input_ids, labels = self.reorg_ids(input_ids, labels, audio_offsets,
-                                           audio_features,
-                                           audio_features_lengths, batch_idx)
         token_length = text_lengths[0].item()
         min_length = token_length * 2
         max_length = token_length * 20
-        # There is no prompt token output if we use `inputs_embeds`
-        # instead of `input_ids`
-        inputs_embeds = self.llm.get_input_embeddings()(input_ids)
+        if inputs_embeds is None:
+            input_ids, labels = self.reorg_ids(input_ids, labels, audio_offsets,
+                                               audio_features,
+                                               audio_features_lengths,
+                                               batch_idx)
+            # There is no prompt token output if we use `inputs_embeds`
+            # instead of `input_ids`
+            inputs_embeds = self.llm.get_input_embeddings()(input_ids)
         model_outputs = self.llm.generate(
             inputs_embeds=inputs_embeds,
             attention_mask=attention_mask,