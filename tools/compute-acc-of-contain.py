34132f164858930843ad5ca5da162b7f99d3da37
cdliang11 <1404056823@qq.com>
2025-09-02 15:00:50 +0800

    [example] add scripts

---
+++ tools/compute-acc-of-contain.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import re
+import string
+import sys
+
+from word2number import w2n
+
+
+def is_string_in_string(text, candidate):
+    return all(x in text for x in candidate.split(" "))
+
+
+def is_list_in_string(text, candidate):
+    return any(
+        [
+            is_string_in_string(text, x)
+            if isinstance(x, str) else is_list_in_string(text, x)
+            for x in candidate
+        ]
+    )
+
+
+def clean_punctuation(value):
+    punctuation = string.punctuation
+    punctuation = punctuation.replace("'", "")
+    value = re.sub(f"[{punctuation}]", " ", value)
+    return value
+
+
+if __name__ == "__main__":
+
+    pred_gt_json_file = sys.argv[1]
+
+    acc = 0
+    num = 0
+
+    with open(pred_gt_json_file, "r") as f:
+        for line in f:
+            pred_gt = json.loads(line.strip())
+
+            pred = pred_gt["hyp"]
+            gt = pred_gt["ref"]
+
+            pred = clean_punctuation(pred)
+            pred = pred.lower()
+
+            if not isinstance(gt, list):
+                gt = [gt,]
+            gt = [clean_punctuation(x) for x in gt]
+            gt = [x.lower().strip() for x in gt]
+
+            try:
+                gt_number = [str(w2n.word_to_num(x.lower())) for x in gt]
+            except Exception as e:
+                print(e)
+                gt_number = gt
+
+            if is_list_in_string(pred, gt):
+                acc += 1
+            elif is_list_in_string(pred, gt_number):
+                acc += 1
+            else:
+                print("======================================================")
+                print("question: ", pred_gt["question"])
+                print("ref=", pred_gt["ref"])
+                print("hyp=", pred_gt["hyp"])
+            num += 1
+
+    print("======================================================")
+    print(f"{acc=}")
+    print(f"{num=}")
+    print("======================================================")
+
+    acc = acc / num * 100
+
+    print("======================================================")
+    print(f"{acc=}")
+    print("======================================================")
---
+++ tools/get_qa_hyp_ref_text.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+
+test_json_path = sys.argv[1]
+output_text_path = sys.argv[2]
+output_hyp_ref_json = sys.argv[3]
+
+text = []
+with open(output_text_path, 'r') as fin:
+    for line in fin:
+        line = line.strip()
+        text.append(line)
+
+
+with open(test_json_path, "r") as fref, open(output_hyp_ref_json, "w") as fjson:
+    for idx, line in enumerate(fref):
+        data = json.loads(line.strip())
+        new_dict = {}
+        new_dict['key'] = data["key"]
+        new_dict['question'] = data['question']
+        new_dict['ref'] = data["ref"]
+        new_dict['hyp'] = text[idx]
+        fjson.write(json.dumps(new_dict, ensure_ascii=False))
+        fjson.write("\n")
